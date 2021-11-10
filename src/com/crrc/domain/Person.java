321c362ad6b7c3153681beaf9059cf5a12b22b36
cc <964480129@qq.com>
2021-11-11 00:16:46 +0800

    Merge branch 'master' of https://github.com/cc5501/git_test01
     Conflicts:
    	src/com/crrc/domain/Person.java

--- src/com/crrc/domain/Person.java
+++ src/com/crrc/domain/Person.java
@@ -3,9 +3,6 @@ package com.crrc.domain;
 public class Person {
     private int age;
     private int name;
-<<<<<<< HEAD
     private int id;
-=======
     private String address;
->>>>>>> 1c06ba633bf23922e4cae50c97057d78a0c41ad7
 }
